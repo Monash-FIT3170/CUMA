b208f27cdddc891bf05b05a6e945a071699f1452
Sok Huot Ear <sear0002@student.monash.edu>
2024-05-11 00:37:05 +1000

    test trial express.js

--- cuma/api/routes/unit.js
+++ cuma/api/routes/unit.js
@@ -4,9 +4,33 @@
 import express from 'express';
 const router = express.Router();
 
-// Define routes for data-related endpoints
-router.get('/', (req, res) => {
-    // Handle GET request to retrieve all data
+// Example route handler accessing the client
+router.get('/', async (req, res) => {
+    try {
+        // Access the MongoDB client from the request object
+        const client = req.client;
+        await client.connect()
+
+        // const database = client.db('sample_mflix');
+        // const movies = database.collection('movies');
+        // // Query for a movie that has the title 'Back to the Future'
+        // const query = { title: 'Back to the Future' };
+        // const movie = await movies.findOne(query);
+
+        const collections  = client.listCollections().toArray();
+
+        // Extract collection names from the collections array
+        const collectionNames = collections.map(collection => collection.name);
+        
+        // Send collection names as the response
+        res.json(collectionNames);
+
+        res.send(movie)
+    } catch (error) {
+        // Handle errors
+        console.error('Error:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
 });
 
 router.get('/:id', (req, res) => {
@@ -24,3 +48,5 @@ router.put('/:id', (req, res) => {
 router.delete('/:id', (req, res) => {
     // Handle DELETE request to delete data by ID
 });
+
+export default router;
--- cuma/api/server.js
+++ cuma/api/server.js
@@ -1,18 +1,35 @@
-const express = require('express');
-
-
-
-// const userRoutes = require('./routes/userRoutes');
-// const productRoutes = require('./routes/productRoutes');
+import express from 'express';
+import { MongoClient } from 'mongodb';
+import unit from './routes/unit.js';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 const port = 3000;
 
-// Mount the route handlers
-app.use('/api/data', unit);
-// app.use('/api/users', userRoutes);
-// app.use('/api/products', productRoutes);
-
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+// Middleware to attach MongoDB client to requests
+app.use((req, res, next) => {
+    req.client = new MongoClient(process.env.MONGODB_URI);
+    next();
 });
+
+// Mount the route handlers
+app.use('/api/unit', unit);
+
+async function run() {
+    try {
+        // Start the Express server
+        app.listen(port, () => {
+            console.log(`Server is running on http://localhost:${port}`);
+        });
+    } finally {
+        // Ensure that the client will close when the application exits
+        // await app.locals.client.close();
+    }
+}
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+run().catch(console.dir);
