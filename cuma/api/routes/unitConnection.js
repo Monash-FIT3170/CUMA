9f02ca76c0141bd87f95dd61c16187e58082149a
dtonda8 <davidbatonda7@gmail.com>
2024-05-13 22:35:22 +1000

    Reconfigure db access to match updated db design

--- cuma/api/routes/unitConnection.js
+++ cuma/api/routes/unitConnection.js
@@ -9,44 +9,39 @@ router.post('/add', async (req, res) => {
         // Access the MongoDB client from the request object
         const client = req.client;
         const database = client.db('CUMA');
-        const universities = database.collection('universities');
+        // const universities = database.collection('universities');
+        const units = database.collection('units');
 
         const { universityNameA, unitCodeA, universityNameB, unitCodeB } = req.body;
         if (universityNameA == universityNameB && unitCodeA == unitCodeB) {
             return res.status(404).json({ error: 'Cannot add connection to self'});
         }
 
-        // Check if universities and unitCode exists
-        const universityA = await universities.findOne({universityName: universityNameA, "units.unitCode": unitCodeA});
-        const universityB = await universities.findOne({universityName: universityNameB, "units.unitCode": unitCodeB});
+        // Check if unitCodes exists
+        const unitA = await units.findOne({universityName: universityNameA, "unitCode": unitCodeA});
+        const unitB = await units.findOne({universityName: universityNameB, "unitCode": unitCodeB});
 
-        if (!universityA) {
-            return res.status(404).json({ error: 'universityA not found'});
+        if (!unitA) {
+            return res.status(404).json({ error: 'unitA not found'});
         }
-        if (!universityB) {
-            return res.status(404).json({ error: 'universityB not found' });
+        if (!unitB) {
+            return res.status(404).json({ error: 'unitB not found' });
         }
 
         // Add connection to A
-        const newConnectionAtoB = { universityName: universityNameB, unitCode: unitCodeB};
-        const filterAtoB = { universityName: universityNameA, "units.unitCode": unitCodeA };
-        const updateAtoB = {$push: { "units.$.connection": newConnectionAtoB} };
-        const existingConnectionA = await universities.findOne({ universityName: universityNameA, "units.unitCode": unitCodeA, "units.connection.universityName": universityNameB, "units.connection.unitCode": unitCodeB });
+        const anyConnectionToB = await units.findOne({ universityName: universityNameA, "unitCode": unitCodeA, "connection._id": unitB._id });
 
         let numChanges = 0;
-        if (!existingConnectionA) {
-            universities.updateOne(filterAtoB, updateAtoB);
+        if (!anyConnectionToB) {
+            units.updateOne({universityName: universityNameA, "unitCode": unitCodeA}, { $push: { "connection": {_id : unitB._id }} });
             numChanges += 1;
         }
         
         // Add connection to B
-        const newConnectionBtoA = { universityName: universityNameA, unitCode: unitCodeA};
-        const filterBtoA = { universityName: universityNameB, "units.unitCode": unitCodeB };
-        const updateBtoA = {$push: { "units.$.connection": newConnectionBtoA} };
-        const existingConnectionB = await universities.findOne({ universityName: universityNameB, "units.unitCode": unitCodeB, "units.connection.universityName": universityNameA, "units.connection.unitCode": unitCodeA });
+        const anyConnectionToA = await units.findOne({ universityName: universityNameB, "unitCode": unitCodeB, "connection._id": unitA._id });
 
-        if (!existingConnectionB) {
-            universities.updateOne(filterBtoA, updateBtoA);
+        if (!anyConnectionToA) {
+            units.updateOne({universityName: universityNameB, "unitCode": unitCodeB}, { $push: { "connection":{ _id : unitA._id } } });
             numChanges += 1;
         }
 
@@ -63,41 +58,36 @@ router.post('/delete', async (req, res) => {
         // Access the MongoDB client from the request object
         const client = req.client;
         const database = client.db('CUMA');
-        const universities = database.collection('universities');
+        // const universities = database.collection('universities');
+        const units = database.collection('units');
 
         const { universityNameA, unitCodeA, universityNameB, unitCodeB } = req.body;
 
-        // Check if universities and unitCode exists
-        const universityA = await universities.findOne({universityName: universityNameA, "units.unitCode": unitCodeA});
-        const universityB = await universities.findOne({universityName: universityNameB, "units.unitCode": unitCodeB});
+        // Check if unitCodes exists
+        const unitA = await units.findOne({universityName: universityNameA, "unitCode": unitCodeA});
+        const unitB = await units.findOne({universityName: universityNameB, "unitCode": unitCodeB});
 
-        if (!universityA) {
-            return res.status(404).json({ error: 'universityA not found'});
+        if (!unitA) {
+            return res.status(404).json({ error: 'unitA not found'});
         }
-        if (!universityB) {
-            return res.status(404).json({ error: 'universityB not found' });
+        if (!unitB) {
+            return res.status(404).json({ error: 'unitB not found' });
         }
 
         // Add connection to A
-        const connectionAtoB = { universityName: universityNameB, unitCode: unitCodeB };
-        const filterAtoB = { universityName: universityNameA, "units.unitCode": unitCodeA };
-        const pullAtoB = { $pull: { "units.$.connection": connectionAtoB } };
-        const existingConnectionA = await universities.findOne({ universityName: universityNameA, "units.unitCode": unitCodeA, "units.connection.universityName": universityNameB, "units.connection.unitCode": unitCodeB });
+        const anyConnectionToB = await units.findOne({ universityName: universityNameA, "unitCode": unitCodeA, "connection._id": unitB._id });
 
         let numChanges = 0;
-        if (existingConnectionA) {
-            universities.updateOne(filterAtoB, pullAtoB);
+        if (anyConnectionToB) {
+            units.updateOne({universityName: universityNameA, "unitCode": unitCodeA}, { $pull: { "connection": {_id : unitB._id }} });
             numChanges += 1;
         }
         
         // Add connection to B
-        const connectionBtoA = { universityName: universityNameA, unitCode: unitCodeA};
-        const filterBtoA = { universityName: universityNameB, "units.unitCode": unitCodeB };
-        const pullBtoA= { $pull: { "units.$.connection": connectionBtoA } };
-        const existingConnectionB = await universities.findOne({ universityName: universityNameB, "units.unitCode": unitCodeB, "units.connection.universityName": universityNameA, "units.connection.unitCode": unitCodeA });
+        const anyConnectionToA = await units.findOne({ universityName: universityNameB, "unitCode": unitCodeB, "connection._id": unitA._id });
 
-        if (existingConnectionB) {
-            universities.updateOne(filterBtoA, pullBtoA);
+        if (anyConnectionToA) {
+            units.updateOne({universityName: universityNameB, "unitCode": unitCodeB}, { $pull: { "connection" : { _id : unitA._id } } });
             numChanges += 1;
         }
 
