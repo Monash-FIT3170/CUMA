96179efb2324c4b660657d0358c1c9873556f854
TeriYeaki <118505370+TeriYeaki@users.noreply.github.com>
2024-08-13 14:46:30 +1000

    refactoring mfa files - combining all mfa script into on

---
+++ cuma/front-end/mfa-authentication.js
@@ -0,0 +1,78 @@
+// Function to render MFA QR code
+function renderMFAQRCode() {
+    // Request MFA setup
+    Backend.Auth.setupMFA().then(response => {
+        console.log(response);
+        if (response.status === 201) {
+            // Extract the imageURL and set the QR Code
+            const { imageUrl, secret } = response;
+            const qrCodeImage = document.getElementById('qrcode');
+
+            if (imageUrl && qrCodeImage) {
+                qrCodeImage.src = imageUrl;
+            }
+        } else {
+            alert("Error " + response.status + ": " + response.error);
+        }
+    }).catch(error => {
+        console.error("An error occurred:", error);
+        // Handle the error, e.g., show an error message to the user
+    });
+}
+
+// Function to handle signup form submission
+function handleVerifyTOTP(e) {
+    e.preventDefault();
+
+    // Get the input token value
+    const token = document.getElementById('totp-code').value;
+    
+    // Request MFA Enable
+    Backend.Auth.enableMFA(token).then(response => {
+        if (response.status === 200) { // Assuming backend uses 200 for success
+            alert("Success: " + response.message);
+            window.location.href = "/login";
+        } else {
+            alert("Error " + response.status + ": " + response.error);
+        }
+    }).catch(error => {
+        console.error("An error occurred:", error);
+        alert("An error occurred: " + error.message);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Get the current URL path
+    const path = window.location.pathname;
+
+    if (path === '/signup/mfa-setup-qr'){
+        // Render MFA QR Code immediately
+        renderMFAQRCode();
+    }
+
+    const enableMFA = document.getElementById('signup-mfa-enable');
+    if (enableMFA) {
+        enableMFA.addEventListener('click', () => {
+            window.location.href = '/signup/mfa-setup-qr';
+        });
+    }
+
+    const skipMFA = document.getElementById('signup-mfa-skip');
+    if (skipMFA) {
+        skipMFA.addEventListener('click', () => {
+            window.location.href = '/login';
+        });
+    }
+
+    const continueMFA = document.getElementById('mfa-setup-continue');
+    if (continueMFA) {
+        continueMFA.addEventListener('click', () => {
+            window.location.href = '/signup/mfa-verify-totp';
+        });
+    }
+
+    const verifyTOTP = document.getElementById('mfa-verify-totp');
+    if (verifyTOTP) {
+        verifyTOTP.addEventListener('click', handleVerifyTOTP);
+    }
+});
