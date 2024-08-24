0faf85a6809d8d50ae110fd85e2c63bdff16d6bd
mishalalhaidar <malh0009@student.monash.edu>
2024-08-25 02:47:59 +1000

    Update nodemon watch for cuma/admin folder. Added mongoose model for pending students and connected mongoose model in adminjs

--- cuma/admin/adminConfig.js
+++ cuma/admin/adminConfig.js
@@ -1,7 +1,22 @@
 import AdminJS from 'adminjs'
 import AdminJSExpress from '@adminjs/express'
+import * as AdminJSMongoose from '@adminjs/mongoose'
+import PendingStudent  from '../api/models/PendingStudentModel.js'
 
-const adminJsOptions = {}
+AdminJS.registerAdapter({
+    Resource: AdminJSMongoose.Resource,
+    Database: AdminJSMongoose.Database,
+  })
+
+const adminJsOptions = {
+    resources: [PendingStudent],
+    rootPath: '/admin',
+    branding: {
+        companyName: 'Cuma Admin',
+        softwareBrothers: false,
+        logo: false
+    },
+}
 
 export const setupAdminJS = async (app) => {
   const admin = new AdminJS(adminJsOptions)
