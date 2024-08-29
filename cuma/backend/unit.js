b581dc5ee8b61a11d4e9dfb491fb084d9bac9e63
chuiwinnie <winniechui0106@gmail.com>
2024-08-29 23:14:13 +1000

    Added AIMatch to unit backend

--- cuma/backend/unit.js
+++ cuma/backend/unit.js
@@ -1,79 +1,72 @@
-const unitBackendPath = "/api/unit"
+const unitBackendPath = "/api/unit";
 
 // define namespace
 Backend.Unit.getAllUnitsFromUniversity = async function (universityName) {
-    /**
-     * @param {string} universityName
-     *
-     * @return {json} API response.
-     */
-    const params = {"universityName": universityName}
+  /**
+   * @param {string} universityName
+   *
+   * @return {json} API response.
+   */
+  const params = { universityName: universityName };
 
-    try {
-        const url = new URL(serverPath + unitBackendPath + "/getAllFromUni");
-        url.search = new URLSearchParams(params).toString();
+  try {
+    const url = new URL(serverPath + unitBackendPath + "/getAllFromUni");
+    url.search = new URLSearchParams(params).toString();
 
-        const response = await fetch(url)
-        const result = await response.json()
-        return result
-    }
-    catch (error) {
-        console.error("Error:", error);
-    }
-}
-
+    const response = await fetch(url);
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error("Error:", error);
+  }
+};
 
 Backend.Unit.getAllUnitsNotInUniversity = async function (universityName) {
-    /**
-     * @param {string} universityName
-     *
-     * @return {json} API response.
-     */
+  /**
+   * @param {string} universityName
+   *
+   * @return {json} API response.
+   */
 
-    const params = { "universityName": universityName }
+  const params = { universityName: universityName };
 
-    try {
-        const url = new URL(serverPath + unitBackendPath + "/getAllNotInUni");
-        url.search = new URLSearchParams(params).toString();
+  try {
+    const url = new URL(serverPath + unitBackendPath + "/getAllNotInUni");
+    url.search = new URLSearchParams(params).toString();
 
-        const response = await fetch(url)
-        const result = await response.json();
-        return result;
-    }
-    catch (error) {
-        console.error("Error:", error);
-    }
-}
-
+    const response = await fetch(url);
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error("Error:", error);
+  }
+};
 
 Backend.Unit.retrieveUnit = async function (universityName, unitCode) {
-    /*
+  /*
         universityName: str
         unitCode: str
     */
 
-    const params = { universityName, unitCode };
+  const params = { universityName, unitCode };
 
-    try {
-        const url = new URL(serverPath + unitBackendPath + "/retrieveUnit");
-        url.search = new URLSearchParams(params).toString();
+  try {
+    const url = new URL(serverPath + unitBackendPath + "/retrieveUnit");
+    url.search = new URLSearchParams(params).toString();
 
-        const response = await fetch(url);
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        const unit = await response.json();
-        return unit;
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
     }
-    catch (error) {
-        console.error("Error:", error);
-    }
-
-}
-
+    const unit = await response.json();
+    return unit;
+  } catch (error) {
+    console.error("Error:", error);
+  }
+};
 
 Backend.Unit.add = async function (universityName, unitInfo) {
-    /**
+  /**
      * @param {string} universityName
      @param {json} unitInfo = {
             name: str;
@@ -89,40 +82,38 @@ Backend.Unit.add = async function (universityName, unitInfo) {
      @return {json} API reponse
      *  */
 
-    // var name=  unitInfo.name;
-    // var desc=  unitInfo.desc;
-    // var type=  unitInfo.type;
-    // var creditPt= unitInfo.creditPt;
-    // var level= unitInfo.level;
-    // var overview= unitInfo.overview;
-    // var link= unitInfo.link;
+  // var name=  unitInfo.name;
+  // var desc=  unitInfo.desc;
+  // var type=  unitInfo.type;
+  // var creditPt= unitInfo.creditPt;
+  // var level= unitInfo.level;
+  // var overview= unitInfo.overview;
+  // var link= unitInfo.link;
 
-    fieldMissing = []
+  fieldMissing = [];
 
+  try {
+    const response = await fetch(serverPath + unitBackendPath, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ universityName: universityName, unitInfo }),
+    });
 
-    try {
-        const response = await fetch(serverPath + unitBackendPath, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ "universityName": universityName, unitInfo }),
-        });
+    // Extract the response code
+    const statusCode = response.status;
 
-        // Extract the response code
-        const statusCode = response.status;
-
-        const result = await response.json();
-        console.log({ result: result, status: statusCode });
-        return { result: result, status: statusCode };
-    } catch (error) {
-        console.log("Error:", error);
-    }
-}
-
+    const result = await response.json();
+    console.log({ result: result, status: statusCode });
+    return { result: result, status: statusCode };
+  } catch (error) {
+    console.log("Error:", error);
+  }
+};
 
 Backend.Unit.modify = async function (universityName, unitCode, newUnitInfo) {
-    /**
+  /**
      * @param {string} universityName
      @param {json} unitInfoChange  - the modification
      {
@@ -141,54 +132,92 @@ Backend.Unit.modify = async function (universityName, unitCode, newUnitInfo) {
      @return {json} API reponse
      *  */
 
-    try {
-        const url = new URL(serverPath + unitBackendPath + "/" + encodeURIComponent(unitCode));
+  try {
+    const url = new URL(
+      serverPath + unitBackendPath + "/" + encodeURIComponent(unitCode)
+    );
 
-        const response = await fetch(url, {
-            method: "PUT",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                "universityName": universityName, newUnitInfo
-            }),
-        });
+    const response = await fetch(url, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        universityName: universityName,
+        newUnitInfo,
+      }),
+    });
 
-        // Extract the response code
-        const statusCode = response.status;
+    // Extract the response code
+    const statusCode = response.status;
 
-        const result = await response.json();
-        return { result: result, status: statusCode };
-    } catch (error) {
-        console.log("Error:", error);
-    }
-}
+    const result = await response.json();
+    return { result: result, status: statusCode };
+  } catch (error) {
+    console.log("Error:", error);
+  }
+};
 
 Backend.Unit.delete = async function (universityName, unitCode) {
-    /**
-     * @param {string} universityName
-     * @param {int} unitCode
-     */
+  /**
+   * @param {string} universityName
+   * @param {int} unitCode
+   */
 
-    try {
-        const url = new URL(serverPath + unitBackendPath + "/" + encodeURIComponent(unitCode));
+  try {
+    const url = new URL(
+      serverPath + unitBackendPath + "/" + encodeURIComponent(unitCode)
+    );
 
-        const response = await fetch(url, {
-            method: "Delete",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                "universityName": universityName
-            }),
-        });
+    const response = await fetch(url, {
+      method: "Delete",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        universityName: universityName,
+      }),
+    });
 
-        // Extract the response code
-        const statusCode = response.status;
+    // Extract the response code
+    const statusCode = response.status;
 
-        const result = await response.json();
-        return { result: result, status: statusCode };
-    } catch (error) {
-        console.log("Error:", error);
-    }
-}
+    const result = await response.json();
+    return { result: result, status: statusCode };
+  } catch (error) {
+    console.log("Error:", error);
+  }
+};
+
+Backend.Unit.AIMatch = async function (unitSRC, unitsToCompare) {
+  /**
+   * @param {JSON} unitSRC
+   * @param {JSON} unitsToCompare
+   *
+   * Makes a post request to server side gemini api code
+   */
+
+  try {
+    const url = new URL(serverPath + unitBackendPath + "/geminiMatch");
+    console.log(`URL: ${url}`);
+
+    console.log("Getting response...");
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        unitSRC: unitSRC,
+        unitsToCompare: unitsToCompare,
+      }),
+    });
+
+    // Extract the response code
+    const statusCode = response.status;
+    const result = await response.json();
+    return { result: result, status: statusCode };
+  } catch (error) {
+    console.log("Error:", error);
+  }
+};
