ed69c44b8e66b6096fb9bb0ae40f59eea8f3ba06
TeriYeaki <118505370+TeriYeaki@users.noreply.github.com>
2024-09-20 23:38:23 +1000

    Add Transfer Planner home to store student saved planner

--- cuma/front-end/Transfer-planner.js
+++ cuma/front-end/Transfer-planner.js
@@ -33,6 +33,103 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 
 
+// ---------- Transfer Planner Home ---------- //
+const maxPlanners = 5;
+const transferPlannerHome = document.getElementById("transfer-planner-home");
+const plannerList = document.getElementById('planner-list');
+const emptyMessage = document.getElementById('empty-message');
+const createButton = document.getElementById('create-planner');
+const createNewPlanModal = document.getElementById('create-new-plan');
+const closeCreateNewPlanModal = document.getElementById('close-create-new-plan-modal');
+
+// Display today's date
+const dateElement = document.getElementById('date');
+const today = new Date();
+const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+dateElement.textContent = today.toLocaleDateString(undefined, options);
+
+// // Load existing planners from local storage or create sample data
+let planners = getSavedTransferPlans();
+renderPlanners();
+
+function getSavedTransferPlans() {
+    // Backend.TransferPlan.getSavedTransferPlans.then(response => { 
+    //     if (response.status == 200) {
+    //         return response.transferPlans;
+    //     } else {
+    //         alert("Error " + response.status + ": " + response.error);
+    //     }
+    // }).catch(error => {
+    //     console.error("An error occurred:", error);
+    // });
+
+    // Mock Data
+    const savedTransferPlan = [
+        {name: 'Oxford Transfer 2025', courseLevel: 'Undergraduate', course: 'Bachelor of Engineering - Software', studyYear: '2026', studyPeriod: 'Semester 1', transferUniversity: 'oxford', connection: {unit1:{home:"MAT1830", target: ""}, unit2:{home:"", target: ""}, unit3:{home:"", target: ""}, unit4:{home:"", target: ""}}},
+        {name: 'Yale Transfer 2025', courseLevel: 'Undergraduate', course: 'Bachelor of Engineering - Software', studyYear: '2024', studyPeriod: 'Semester 2', transferUniversity: 'yale', connection: {unit1:{home:"MAT1830", target: ""}, unit2:{home:"", target: ""}, unit3:{home:"", target: ""}, unit4:{home:"", target: ""}}},
+        {name: 'MIT Winter Transfer 2025', courseLevel: 'Undergraduate', course: 'Bachelor of Engineering - Software', studyYear: '2022', studyPeriod: 'Semester 1', transferUniversity: 'mit', connection: {unit1:{home:"MAT1830", target: ""}, unit2:{home:"", target: ""}, unit3:{home:"", target: ""}, unit4:{home:"", target: ""}}},
+        {name: 'Harvard Semester 2 2025', courseLevel: 'Undergraduate', course: 'Bachelor of Engineering - Software', studyYear: '2023', studyPeriod: 'Semester 2', transferUniversity: 'harvard', connection: {unit1:{home:"MAT1830", target: ""}, unit2:{home:"", target: ""}, unit3:{home:"", target: ""}, unit4:{home:"", target: ""}}}
+    ];
+
+    return savedTransferPlan;
+}
+
+function openTransferPlan() {
+
+}
+
+function deleteTransferPlan() {
+
+}
+
+function createNewTransferPlan() {
+
+}
+
+createButton.addEventListener('click', () => {
+    createNewPlanModal.classList.remove('hidden');
+});
+
+closeCreateNewPlanModal.addEventListener('click', () => {
+    createNewPlanModal.classList.add('hidden');
+})
+
+// Render the list of Planners
+function renderPlanners() {
+
+    plannerList.innerHTML = '';
+
+    if (planners.length === 0) {
+        emptyMessage.classList.remove('hidden');
+        plannerList.classList.add('hidden');
+    } else {
+        emptyMessage.classList.add('hidden');
+        plannerList.classList.remove('hidden');
+
+        planners.forEach((planner, index) => {
+            const plannerItem = document.createElement('div');
+            plannerItem.className = 'planner-item';
+            plannerItem.innerHTML = `
+                <span>${planner.name}</span>
+                <button onclick="deletePlanner(${index})">🗑️</button>
+            `;
+            plannerItem.addEventListener("click", () => {
+                // TODO: Add Click event to 
+                console.log(`${planner.name} is clicked`)
+            })
+            plannerList.appendChild(plannerItem);
+        });
+    }
+}
+
+window.deletePlanner = function(index) {
+    planners.splice(index, 1);
+    localStorage.setItem('planners', JSON.stringify(planners));
+    renderPlanners();
+}
+
+
+
 // ---------- Create New Planner Logic ---------- //
 
 
@@ -92,8 +189,9 @@ function configureTransferPlanner(formData) {
     plannerTargetUniName.textContent = formData.transferUniversity;
 
     // Change container visibility
-    createNewPlanContainer.style.display = "none";
-    plannerContainer.style.display = "flex";
+    transferPlannerHome.classList.add("hidden");
+    createNewPlanContainer.classList.add("hidden");
+    plannerContainer.classList.remove("hidden");
 
     // Congfigure Unit Slots
     configureHomeUnitSlot('Monash');
