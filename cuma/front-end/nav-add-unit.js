257526ad88eeed7b0062fe077af0edcb9ecb19a6
Sok Huot Ear <sear0002@student.monash.edu>
2024-09-27 19:29:32 +1000

    addeed a loading screen

--- cuma/front-end/nav-add-unit.js
+++ cuma/front-end/nav-add-unit.js
@@ -2,6 +2,8 @@ const form = document.getElementById('scan-url-form');
 
 const scanButton = document.getElementById('scan-url-button');
 
+
+
 // Add event listener to the button
 scanButton.addEventListener('click', function(event) {
     // Prevent the default form submission behavior
@@ -9,12 +11,31 @@ scanButton.addEventListener('click', function(event) {
 
     // Get the value of the URL input
     const urlInput = document.getElementById('url').value;
+    mainPage = document.getElementById("main");
 
     // Check if the input is not empty
     if (urlInput.trim() !== "") {
+
+        // waiting screen
+        mainPage.innerHtml += waitingScreen
+
+        // call web scraper
         Backend.Misc.scrapeDomestic(urlInput)
     } else {
         alert("Input is empty.")
     }
+
+
+    const waitingScreen = `        <div class="waiting-screen">
+    <div class="spinner"></div>
+    <p>Loading, please wait...</p>
+    </div>`
+
+    // remove waiting screen
+    main.querySelector('.waiting-screen').remove();
+
+    
+
+
 
 })
