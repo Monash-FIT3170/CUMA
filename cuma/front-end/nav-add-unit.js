010c9985a378d7b92a5405c3184396e7e52ef737
Sok Huot Ear <sear0002@student.monash.edu>
2024-09-27 23:21:45 +1000

    added UI interactivity

--- cuma/front-end/nav-add-unit.js
+++ cuma/front-end/nav-add-unit.js
@@ -2,7 +2,13 @@ const form = document.getElementById('scan-url-form');
 
 const scanButton = document.getElementById('scan-url-button');
 
-
+    const waitingScreen = `
+            <div class="waiting-screen" id="waiting-screen">
+            <div class="spinner" id="spinner"></div>
+            <p id="waiting-screen-text">Loading, please wait...</p>
+            <button disabled id="waiting-screen-done-button" onclick="closeWaitingScreen()">Done</button>
+            </div>
+    `
 
 // Add event listener to the button
 scanButton.addEventListener('click', function(event) {
@@ -11,13 +17,16 @@ scanButton.addEventListener('click', function(event) {
 
     // Get the value of the URL input
     const urlInput = document.getElementById('url').value;
-    mainPage = document.getElementById("main");
+    
+
+    const mainPage = document.getElementById("main");
+    mainPage.innerHTML += waitingScreen
 
     // Check if the input is not empty
     if (urlInput.trim() !== "") {
 
-        // waiting screen
-        mainPage.innerHtml += waitingScreen
+        // insert waiting screen
+
 
         // call web scraper
         Backend.Misc.scrapeDomestic(urlInput)
@@ -26,16 +35,12 @@ scanButton.addEventListener('click', function(event) {
     }
 
 
-    const waitingScreen = `        <div class="waiting-screen">
-    <div class="spinner"></div>
-    <p>Loading, please wait...</p>
-    </div>`
-
-    // remove waiting screen
-    main.querySelector('.waiting-screen').remove();
-
     
 
 
-
 })
+
+function closeWaitingScreen() {
+    const screen = document.getElementById("waiting-screen")
+    screen.remove()
+}
