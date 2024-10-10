2d1f1f3dfcfc875e8d5417cd88765d51a73dbef3
maddyyy0110 <tpra0008@student.monash.edu>
2024-10-10 15:44:30 +1100

    Added new section to populate with unit information after succesful search

--- cuma/front-end/nav-unit-info.js
+++ cuma/front-end/nav-unit-info.js
@@ -3,8 +3,12 @@ function addButtonFunc(){
     let button = document.getElementById("searchButton");
 
 
-
     button.addEventListener("click", async () => {
+
+        let defaultHTML = `
+            <h2>Selected Unit Information</h2>
+            <p id="display-unit-description">Search for a unit to see the details</p>
+        `;
 
         if (document.getElementById("errorText").style.display === "inline"){
             toggleErrorSpan();
@@ -12,12 +16,37 @@ function addButtonFunc(){
 
         let uniForm = document.getElementById("search-institution");
         let unitForm = document.getElementById("search-unit");
-        console.log("hi")
+        
 
         let result = await Backend.Unit.retrieveUnit(uniForm.value, unitForm.value)
 
         if (result == undefined){
             toggleErrorSpan()
+            document.getElementById("displayUnitSection").innerHTML = defaultHTML;
+        } else {
+            let unitName = result.unitName;
+            let unitCode = result.unitCode;
+            let unitType = result.unitType;
+            let creditPoints = result.creditPoints;
+            let unitLevel = result.unitLevel;
+            let unitDescription = result.unitDescription;
+            let handBookURL = result.handBookURL;
+
+
+            let newHTML = `
+                <h2>Selected Unit Information</h2>
+                <h3 id="display-unit-title">${unitName}</h3>
+                <p><strong>Code:</strong> <span id="display-unit-id">${unitCode}</span></p>
+                <p><strong>Type:</strong> <span id="display-unit-type">${unitType}</span></p>
+                <p><strong>Credits:</strong> <span id="display-unit-credits">${creditPoints}</span></p>
+                <p><strong>Level:</strong> <span id="display-unit-level">${unitLevel}</span></p>
+                <p id="display-unit-description">${unitDescription}</p>
+                <p><strong>Handbook URL:</strong> <a href="${handBookURL}">Handbook Page</a></p>
+            `;
+
+
+            
+            document.getElementById("displayUnitSection").innerHTML = newHTML;
         }
 
         console.log(result)
