bea98ad79b977bb31a33a4885633c45c7a3c751e
maddyyy0110 <tpra0008@student.monash.edu>
2024-10-10 20:37:49 +1100

    Added on click functionality to add unit submit button. Now calls Backend.Unit.add method

--- cuma/front-end/nav-add-unit.js
+++ cuma/front-end/nav-add-unit.js
@@ -33,12 +33,55 @@ scanButton.addEventListener('click', function(event) {
     } else {
         alert("Input is empty.")
     }
-
-
-    
-
-
 })
+
+const addUnitButton = document.querySelector("form[id='add-unit-form'] > button");
+
+//add functionality for add unit button
+addUnitButton.addEventListener('click', async function(event){
+    // Prevent the default form submission behavior
+    event.preventDefault();
+    console.log("hi")
+
+    //get user inputs
+    const institution = document.getElementById("institution").value;
+    const unitCode = document.getElementById("unit-code").value;
+    const unitTitle = document.getElementById("unit-title").value; 
+    const unitType = document.getElementById("unit-type").value; 
+    const creditPoints = document.getElementById("credit-points").value; 
+    const unitLevel = document.getElementById("unit-level").value; 
+    const faculty = document.getElementById("unit-faculty").value;
+    let offering = document.getElementById("unit-offering").value;
+    const description = document.getElementById("unit-description").value;
+    const handbookURL = document.getElementById("handbookURL").value; 
+
+    //check if multiple offerings where provided
+    if (offering.includes(',')){
+        offering = offering.split(",")
+    } else {
+        offering = [offering]
+    }
+
+    //format payload
+    const payload = {
+        "universityName": institution,
+        "unitCode": unitCode,
+        "unitName": unitTitle,
+        "unitDescription": description,
+        "unitType": unitType,
+        "unitLevel": unitLevel,
+        "creditPoints": creditPoints,
+        "course": [],
+        "faculty": faculty,
+        "offering": offering,
+        "handBookURL": handbookURL
+    };
+
+    //post to mongoDB
+    result = await Backend.Unit.add(institution,payload)
+
+    console.log(result)
+});
 
 function closeWaitingScreen() {
     // const screen = document.getElementById("waiting-screen")
