a72506b25a69ad8302461f5be8f2c97844245026
maddyyy0110 <tpra0008@student.monash.edu>
2024-10-10 14:58:35 +1100

    Added handling for invalid inputs, displays text message in toggleable span.

--- cuma/front-end/nav-unit-info.js
+++ cuma/front-end/nav-unit-info.js
@@ -5,14 +5,33 @@ function addButtonFunc(){
 
 
     button.addEventListener("click", async () => {
+
+        if (document.getElementById("errorText").style.display === "inline"){
+            toggleErrorSpan();
+        }
+
         let uniForm = document.getElementById("search-institution");
         let unitForm = document.getElementById("search-unit");
         console.log("hi")
 
         let result = await Backend.Unit.retrieveUnit(uniForm.value, unitForm.value)
 
+        if (result == undefined){
+            toggleErrorSpan()
+        }
+
         console.log(result)
     });
+}
+
+function toggleErrorSpan(){
+    let span = document.getElementById("errorText");
+
+    if (span.style.display === "none"){
+        span.style.display = "inline";
+    } else if (span.style.display === "inline"){
+        span.style.display = "none";
+    }
 }
 
 addButtonFunc()
