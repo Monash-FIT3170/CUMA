4c2a437e3321828d790e7398d478efdc87be7a04
SokhuotEar <sear0002@student.monash.edu>
2024-10-10 15:09:52 +1100

    added a modal for adding custom unit

---
+++ cuma/front-end/transfer-planner-mapping-modal.js
@@ -0,0 +1,6 @@
+function openModal(){
+    console.log("hello")
+    const addCustomUnitModal = document.getElementById("add-custom-unit-modal");
+    addCustomUnitModal.style.display = "block";
+    // overlay.style.display = "block";
+}
